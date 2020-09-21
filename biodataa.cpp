db942a946c35bfc1007775e06d92e89a2e1e733a
AchmadArsa038 <arsaachmad12345@gmail.com>
2020-09-21 10:33:40 +0700

    first commit

---
+++ biodataa.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+int main()
+{
+	printf("\BIODATA PRIBADI\n\n");
+	printf("Nama			: Achmad Arsa Abdillah Madjid\n");
+	printf("NPM			: 19081010038\n");
+	printf("Kelas			: PEMLAN C\n");
+	printf("Prodi			: Informatika\n");
+	printf("Tanggal Lahir		: 06 Agustus 2000\n");
+	getch();
+}
